af5e0ff704f3a76ce068406b39d1b859b80686d9
fahmi95 <fahmi.sy@gmail.com>
2024-11-23 18:17:55 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,17 @@
+const script1 = document.createElement('script');
+script1.type = 'text/javascript';
+script1.text = `
+    atOptions = {
+        'key': '391a7c454c7632fa62a39c1549db1f6d',
+        'format': 'iframe',
+        'height': 250,
+        'width': 300,
+        'params': {}
+    };
+`;
+document.body.appendChild(script1);
+
+const script2 = document.createElement('script');
+script2.type = 'text/javascript';
+script2.src = '//trashyepochabsorbing.com/391a7c454c7632fa62a39c1549db1f6d/invoke.js';
+document.body.appendChild(script2);
